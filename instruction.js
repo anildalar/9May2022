3cb194b9672a91d9aaaaa20177aff764cb395b6c
“anildalar” <dollorinfotech@gmail.com>
2022-05-09 10:19:26 +0530

    v1

---
+++ instruction.js
@@ -0,0 +1,27 @@
+React Router Dom V6
+
+
+<Outlet> Component
+
+
+<Layout>
+		<div></div>
+</Layout>
+
+{props.childres}
+
+<BrowserRouter>
+		<Routes>
+				<Route element={Layout}>
+						<Route></Route>
+				</Route>
+				<Route />
+		</Routes>
+</BrowserRouter>
+
+Route Comp can contain another route comp
+
+
+/about -> about
+
+/home  -> home
